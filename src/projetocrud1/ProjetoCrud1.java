2649620f044f84ac1f5228e42bff5f38e6bb6e61
CoracaoDeLeao <isaacdecarvalho2005@gmail.com>
2023-09-23 15:17:50 -0300

    .

--- src/projetocrud1/ProjetoCrud1.java
+++ src/projetocrud1/ProjetoCrud1.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package projetocrud1;
 
 /**
@@ -14,10 +10,10 @@ public class ProjetoCrud1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
 
         String nome;
-        //teste
-        //muddaikwd
+
+        
     }
 }
