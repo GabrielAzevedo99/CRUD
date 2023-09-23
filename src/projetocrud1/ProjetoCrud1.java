7a9b3706b63f3b89e24bb1af8d7b0755ca24b056
CoracaoDeLeao <isaacdecarvalho2005@gmail.com>
2023-09-23 14:59:39 -0300

    AAA

--- src/projetocrud1/ProjetoCrud1.java
+++ src/projetocrud1/ProjetoCrud1.java
@@ -16,7 +16,7 @@ public class ProjetoCrud1 {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        String nome;
+        String nomee;
     }
     
 }
