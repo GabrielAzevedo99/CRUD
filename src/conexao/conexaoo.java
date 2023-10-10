b276cd64fa859628bdc86019bdcd7d5ba26f574d
CoracaoDeLeao <isaacdecarvalho2005@gmail.com>
2023-10-10 18:36:30 -0300

    Método para leitura de registros da(s) tabela(s)

--- src/conexao/conexaoo.java
+++ src/conexao/conexaoo.java
@@ -3,38 +3,35 @@ package conexao;
 import java.sql.*;
 
 public class conexaoo {
+    
+    //private String bd = null;
+    private String URL = null;
+    private static final String USUARIO = "root";
+    private static final String SENHA = "admin";
+    
+    public conexaoo() {
+        this.URL = "jdbc:mysql://localhost:3307/";
+    }
+    
+    public conexaoo(String base) {
+        this.URL = "jdbc:mysql://localhost:3307/" + base;
+    }
 
-    /*public static void main(String[] args) {
-
-        conexao conexaoo = new conexao();
-        Connection conexao = conexaoo.getConnection();
-
-    }*/
-
-    public Connection getConnection() {
+    public Connection getConnection() throws SQLException {
 
         Connection conexao = null;
-        // URL de conexão com o banco de dados MySQL
-        String url = "jdbc:mysql://localhost:3307/agenda";
-
-        // Nome de usuário e senha do banco de dados
-        String username = "root";
-        String password = "admin";
 
         try {
             // Carrega o driver JDBC do MySQL (você deve ter o JAR do driver no seu classpath)
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // Estabelece a conexão com o banco de dados
-            conexao = DriverManager.getConnection(url, username, password);
+            conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
 
             System.out.println("Conectou no banco de dados.");
 
         } catch (ClassNotFoundException e) {
             System.out.println("Erro: Não encontrou o driver do BD!");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.out.println("Erro: Não conseguiu conectar no BD!");
             e.printStackTrace();
         }
         return conexao;
--- src/getMetaData/getMetaData.java
+++ src/getMetaData/getMetaData.java
@@ -7,100 +7,141 @@ package getMetaData;
 import conexao.conexaoo;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class getMetaData {
-    
-    public static void main(String[] args) {
-        
-        GetSCHEMA();
-        
-    }
-    
-    public static void GetSCHEMA() {
-    
-        conexaoo getConexao = new conexaoo();
-        String catalogName;
-        
-        try (Connection conexao = getConexao.getConnection()) {
-            
-            DatabaseMetaData metaData = conexao.getMetaData();
 
-            // Obtenha os catálogos (que podem ser equivalentes aos schemas em alguns bancos de dados)
-            ResultSet catalogs = metaData.getCatalogs();
+    public static void main(String[] args) throws SQLException {
 
-            while (catalogs.next()) {
-                //Pega o nome dos catalogos do banco de dados
-                catalogName = catalogs.getString("TABLE_CAT");
-                
-                System.out.println("Nome da catalog: " + catalogName);
-                
-                getTABELAS(catalogName);
-            }
-            catalogs.close();
-        } 
-        
-        
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        List<String> bases = GetSCHEMA();
+        String base = bases.get(0);
+        System.out.println(base);
+
+        List<String> tabelas = getTABELAS(base);
+        String tabela = tabelas.get(1);
+        System.out.println(tabela);
+
+        List<String> Nomes = getAtributo(base, tabela);
+        String nome = Nomes.get(4);
+
+        List<String> Tipos = getTipoAtributo(base, tabela);
+        String tipo = Tipos.get(4);
+
+        System.out.println("   Coluna: " + nome + "   Tipo: " + tipo);
+
+        List<String> Registros = getRegistros(base, tabela, nome);
+        String Registro = Registros.get(0);
+
+        System.out.println("   Registro: " + Registro);
     }
 
-    public static void getTABELAS(String catalogName) {
+    public static List<String> GetSCHEMA() throws SQLException {
 
+        //conexaoo getConexao = new conexaoo();
         conexaoo getConexao = new conexaoo();
-        
-        int n = 0;
-        
-        String NomeTabela = null;
+        Connection connect = getConexao.getConnection();
+        Statement statement = connect.createStatement();
+        ResultSet resultSet = statement.executeQuery("SHOW DATABASES WHERE `Database` NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys', 'phpmyadmin')");
+        List<String> bases = new ArrayList<>();
 
-        try (Connection conexao = getConexao.getConnection()) {
-            
-            DatabaseMetaData metaData = conexao.getMetaData();
-            
-            String[] types = {"TABLE"};
-            
-            //Busca as tabelas do banco de dados
-            ResultSet tabelas = metaData.getTables(catalogName, null, "%", types);
-
-            while (tabelas.next()) {
-                //Pega o nome das tabelas do banco de dados
-                NomeTabela = tabelas.getString("TABLE_NAME");
-
-                System.out.println("Nome da tabela: " + NomeTabela);
-
-                //n++;
-                getCAMPOS(NomeTabela);
-            }
-            
-            //System.out.println("Número de tabelas: " + n);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        while (resultSet.next()) {
+            String catalogName = resultSet.getString(1);
+            bases.add(catalogName);
         }
+
+        return bases;
     }
-    
-    public static void getCAMPOS(String NomeTabela) {
-        
-        conexaoo getConexao = new conexaoo();
-        
-        try (Connection conexao = getConexao.getConnection()) {
-            
-            DatabaseMetaData metaData = conexao.getMetaData();
-            
-            // Obtenha as colunas da tabela
-            ResultSet columns = metaData.getColumns(null, null, NomeTabela, null);
 
-            while (columns.next()) {
+    public static List<String> getTABELAS(String schema) throws SQLException {
 
-            String columnName = columns.getString("COLUMN_NAME");
-            String columnType = columns.getString("TYPE_NAME");
-            System.out.println("   Coluna: " + columnName + ", Tipo: " + columnType);
-            }
+        conexaoo getConexao = new conexaoo(schema);
+        Connection connect = getConexao.getConnection();
+        Statement statement = connect.createStatement();
+        ResultSet resultSet = statement.executeQuery("SHOW TABLES");
+        List<String> tabelas = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String tabela = resultSet.getString(1);
+            tabelas.add(tabela);
+        }
+
+        return tabelas;
+    }
+
+    public static List<String> getAtributo(String schema, String tabela) throws SQLException {
+
+        conexaoo getConexao = new conexaoo(schema);
+        Connection connect = getConexao.getConnection();
+        //Statement statement = connect.createStatement();
+
+        PreparedStatement pstmt = connect.prepareStatement("SELECT COLUMN_NAME AS Atributo FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE ?");
+
+        pstmt.setString(1, tabela);
+        //pstmt.setString(2, tabela);
+
+        //ResultSet resultSet = statement.executeQuery("SELECT COLUMN_NAME AS Atributo, DATA_TYPE AS Tipo_de_atributo FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA LIKE (\"schema\") AND TABLE_NAME LIKE (\"tabela\")");
+        ResultSet resultSet = pstmt.executeQuery();
+
+        List<String> atributos = new ArrayList<>();
+        //List<String> tipos = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String atributo = resultSet.getString(1);
+            //String tipo = resultSet.getString(2);
+            atributos.add(atributo);
+            //tipos.add(tipo);
+        }
+
+        /*for(String nome : atributos){
                 
-            
+            System.out.println("   Coluna: " + nome);
+
+        }*/
+        return atributos;
+    }
+
+    public static List<String> getTipoAtributo(String schema, String tabela) throws SQLException {
+
+        conexaoo getConexao = new conexaoo(schema);
+        Connection connect = getConexao.getConnection();
+        //Statement statement = connect.createStatement();
+
+        PreparedStatement pstmt = connect.prepareStatement("SELECT DATA_TYPE AS Tipo_de_atributo FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME LIKE ?");
+
+        pstmt.setString(1, tabela);
+        //pstmt.setString(2, tabela);
+
+        //ResultSet resultSet = statement.executeQuery("SELECT COLUMN_NAME AS Atributo, DATA_TYPE AS Tipo_de_atributo FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA LIKE (\"schema\") AND TABLE_NAME LIKE (\"tabela\")");
+        ResultSet resultSet = pstmt.executeQuery();
+
+        List<String> tipos = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String tipo = resultSet.getString(1);
+            tipos.add(tipo);
         }
-        catch (Exception e) {
-            e.printStackTrace();
+        return tipos;
+    }
+
+    public static List<String> getRegistros(String schema, String tabela, String nome) throws SQLException {
+
+        conexaoo getConexao = new conexaoo(schema);
+        Connection connect = getConexao.getConnection();
+        //Statement statement = connect.createStatement();
+
+        PreparedStatement pstmt = connect.prepareStatement("SELECT " + nome + " FROM " + tabela);
+
+        //pstmt.setString(1, nome);
+        //pstmt.setString(2, tabela);
+        ResultSet resultSet = pstmt.executeQuery();
+
+        List<String> registros = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String registro = resultSet.getString(1);
+            registros.add(registro);
         }
+        return registros;
     }
 }
